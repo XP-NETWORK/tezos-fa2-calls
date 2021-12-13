02b7d246d7c7d2f02c570fdf13728f6ade9f64bf
Xueming Zheng <rocmaildev@gmail.com>
2021-12-13 18:58:25 +0300

    initial

--- contracts/collatz.py
+++ contracts/collatz.py
@@ -13,12 +13,12 @@ class Collatz(sp.Contract):
             xpnft_contract=xpnft_address,
             nft_cnt=sp.nat(0)
         )
-    
+
     @sp.entry_point
     def mint(self, params):
         sp.set_type(params.to, sp.TAddress)
         sp.set_type(params.data, sp.TBigMap(sp.TString, sp.TBytes))
-        
+
         tk = sp.TRecord(
             address=sp.TAddress,
             amount=sp.TNat,
@@ -27,7 +27,8 @@ class Collatz(sp.Contract):
             ),
             token_id=sp.TNat
         )
-        mint_entrypoint = sp.contract(tk, self.data.xpnft_contract, "mint").open_some()
+        mint_entrypoint = sp.contract(
+            tk, self.data.xpnft_contract, "mint").open_some()
         mint_args = sp.record(
             address=params.to,
             amount=sp.nat(1),
@@ -35,17 +36,17 @@ class Collatz(sp.Contract):
             token_id=self.data.nft_cnt
         )
         call(mint_entrypoint, mint_args)
-        
+
         self.data.nft_cnt += sp.nat(1)
 
 
 @sp.add_test(name="collatz_test")
 def test():
     dotenv.load_dotenv(".env")
-    
+
     scenario = sp.test_scenario()
     scenario.h1("Collatz - Inter-Contract Calls")
-    
+
     xpnft_address = os.environ.get("XPNFT_ADDRESS")
     collatz = Collatz(
         xpnft_address=sp.address(xpnft_address)
@@ -55,6 +56,7 @@ def test():
         to=sp.address(os.environ.get("XPNFT_ADMIN")),
         data=sp.utils.metadata_of_url("https://example.com")
     )
+
 
 if __name__ == '__main__':
     dotenv.load_dotenv(".env")
